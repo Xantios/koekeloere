11f12e9cfe72c226cadc27465b21ba7c2c7fd9f8
Xantios Krugor <xantios@MacBook-Air-van-Xantios.local>
2021-11-18 13:29:05 +0100

    prefer port over get for webhook

--- moffel/http.go
+++ moffel/http.go
@@ -1,6 +1,8 @@
 package moffel
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	client "net/http"
 	"net/url"
@@ -13,9 +15,13 @@ import (
 func http(eventType string, fileName string, url *url.URL) error {
 
 	// Post req
+	_, err := httpPost(url, eventType, fileName)
+	if err == nil {
+		return nil
+	}
 
 	// Get Req
-	_, err := httpGet(url, eventType, fileName)
+	_, err = httpGet(url, eventType, fileName)
 	if err != nil {
 		return err
 	}
@@ -42,12 +48,34 @@ func httpGet(url *url.URL, event string, file string) (string, error) {
 	var out []byte
 	_, e := resp.Body.Read(out)
 	if e != nil {
-		return "", err
+		return "", e
 	}
 
 	return string(out), nil
 }
 
-func httpPost() {
-	//
+func httpPost(url *url.URL, event string, file string) (string, error) {
+	values := map[string]string{"name": file, "event": event}
+	jsonTmp, err := json.Marshal(values)
+
+	if err != nil {
+		return "", errors.New("cant encode request data")
+	}
+
+	resp, err := client.Post(url.String(), "application/json", bytes.NewBuffer(jsonTmp))
+	if err != nil {
+		return "", err
+	}
+
+	var out []byte
+	_, e := resp.Body.Read(out)
+	if e != nil {
+		return "", e
+	}
+
+	if resp.StatusCode != 200 {
+		return "", errors.New(resp.Status)
+	}
+
+	return string(out), nil
 }
